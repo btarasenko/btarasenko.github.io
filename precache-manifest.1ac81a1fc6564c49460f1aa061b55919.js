5b9beaa30e6696cfa3b4b0b00185c003ab5596e9
bogdan <apogel@yandex.ru>
2020-06-25 18:19:21 +0300

    build

---
+++ precache-manifest.1ac81a1fc6564c49460f1aa061b55919.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fceb0f0e3919a51d606a01d14c9f6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa2cebc69b74dbf93076",
+    "url": "./static/css/main.982b2509.chunk.css"
+  },
+  {
+    "revision": "d24338dcf302b5f80f36",
+    "url": "./static/js/2.231a979c.chunk.js"
+  },
+  {
+    "revision": "fa2cebc69b74dbf93076",
+    "url": "./static/js/main.4a3a400a.chunk.js"
+  },
+  {
+    "revision": "0aaead598bce510ba5b1",
+    "url": "./static/js/runtime~main.4adebabb.js"
+  },
+  {
+    "revision": "d6e53ddf6f43bc5457ea10d3c1d9aa54",
+    "url": "./static/media/arrow.d6e53ddf.gif"
+  },
+  {
+    "revision": "284736c4b65b038a78528e4828877c3a",
+    "url": "./static/media/arrowswide.284736c4.gif"
+  },
+  {
+    "revision": "17766bb780bbb555f3aeda3e381cd666",
+    "url": "./static/media/bang.17766bb7.gif"
+  },
+  {
+    "revision": "e5a899b4cc68c6446078751489c4942d",
+    "url": "./static/media/diet.e5a899b4.svg"
+  },
+  {
+    "revision": "3c0a11254a3b2480439351986804e523",
+    "url": "./static/media/flower.3c0a1125.gif"
+  },
+  {
+    "revision": "fbeed59365b53c79fe177fa74a0e79db",
+    "url": "./static/media/head.fbeed593.svg"
+  },
+  {
+    "revision": "aeb0a45575624b7a69ce3d1de3d485b6",
+    "url": "./static/media/loop.aeb0a455.gif"
+  },
+  {
+    "revision": "99bf236bdcaebeb7a35c42016f690066",
+    "url": "./static/media/massage1.99bf236b.svg"
+  },
+  {
+    "revision": "2aeb5ab39553aa0033b035c8700c1bcd",
+    "url": "./static/media/massage2.2aeb5ab3.svg"
+  },
+  {
+    "revision": "178f811e20c74b8bf2cb74464e77a351",
+    "url": "./static/media/matrix.178f811e.gif"
+  },
+  {
+    "revision": "b1368a0603c76c4fe7d714a226243cfd",
+    "url": "./static/media/rfbodylifting.b1368a06.svg"
+  },
+  {
+    "revision": "cd18ed86a6fe6a52bba152d3d5f6e185",
+    "url": "./static/media/rffacelift.cd18ed86.svg"
+  },
+  {
+    "revision": "74f779503f2bdfc2f34e72052cae3b3c",
+    "url": "./static/media/star.74f77950.gif"
+  },
+  {
+    "revision": "ce0ea194923a7e636c9e49bcf55e714f",
+    "url": "./static/media/sun.ce0ea194.gif"
+  },
+  {
+    "revision": "555889a7c84f663267d7b748db498df1",
+    "url": "./static/media/wave.555889a7.gif"
+  }
+]);
